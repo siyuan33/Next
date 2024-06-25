55f74e8de758323098ce8a060b976acd8c929efc
DESKTOP-SKLCCK8\Administrator <370455671@qq.com>
2024-06-25 21:28:09 +0800

    Loading 组件

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,3 +1,10 @@
-export default function Page() {
-  return <h1>Hello, Dashboard!</h1>;
+async function getData() {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  return {
+    message: 'Hello, Dashboard!',
+  }
+}
+export default async function DashboardPage(props) {
+  const { message } = await getData()
+  return <h1>{message}</h1>
 }
